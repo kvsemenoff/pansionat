884632c558dfb9986abde640182df88105763761
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-25 18:20:14 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿
+	<?php require_once('includes/header.php'); ?>
+	<?php require_once('includes/header-top.php'); ?>
+	<?php require_once('includes/pod_shapkoy.php'); ?>
+	<?php require_once('includes/dom.php'); ?>
+	<?php require_once('includes/napr.php'); ?>
+	<?php require_once('includes/quests.php'); ?>
+	<?php require_once('includes/news.php'); ?>
+	<?php require_once('includes/foto.php'); ?>
+	<?php require_once('includes/docs.php'); ?>
+	<?php require_once('includes/faq.php'); ?>
+	<?php require_once('includes/review.php'); ?>
+	<?php require_once('includes/about.php'); ?>
+	<?php require_once('includes/sotrud.php'); ?>
+	<?php require_once('includes/partners.php'); ?>
+	<?php require_once('includes/map.php'); ?>
+	<?php require_once('includes/footer.php'); ?>
+
+
+	
