1435f0bd144d9d2948b4c9561d94a1c329837a5c
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-31 13:06:30 +0300

    d

--- includes/header.php
+++ includes/header.php
@@ -7,10 +7,10 @@
 
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
-	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
 	<link rel="stylesheet" href="libs/slick/slick.css" />
-	<link rel="stylesheet" href="libs/wow/animate.css">
+	
 	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
 
 	<link rel="shortcut icon" href="favicon.ico" />
@@ -24,15 +24,9 @@
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
 	<script src="libs/slick/slick.min.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/wow/wow.min.js"></script>
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
 
-	<script>
-    	new WOW().init();
-    </script>
 
-	<script src="js/responsiveTabs.js"></script>
-	<script src="js/jquery.maskedinput.min.js"></script>
+	
+
+	
 	<script src="js/common.js"></script>
