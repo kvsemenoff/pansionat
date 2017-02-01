34d04cf299a2532fd872cb445dd028609d40d235
Konstantin Semenov <kvsemenoff@mail.ru>
2017-02-01 12:51:57 +0300

    d

--- includes/map.php
+++
@@ -1,5 +0,0 @@
-<section class="section mape">
-	<div class="mape-wrap">
-		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=iijMBuwdToBZ6fpia6jaxT5U2PC8Vkkr&amp;width=100%25&amp;height=445&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
-	</div>
-</section>
